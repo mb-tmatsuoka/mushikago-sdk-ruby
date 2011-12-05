9f9cddc974e838392365f10e030b1750a96a5f19
tmatsuoka <tmatsuoka@fa3f8e5a-24df-40e0-b2f8-b43c35aaeae2>
2011-12-05 06:52:03 +0000

    hotaru追加準備
    git-svn-id: svn+ssh://svn.miningbrownie.co.jp/srv/svn/mushikago/sdk/ruby/branches/0.2@922 fa3f8e5a-24df-40e0-b2f8-b43c35aaeae2

--- lib/mushikago/hotaru.rb
+++ lib/mushikago/hotaru.rb
@@ -3,5 +3,19 @@ module Mushikago
   module Hotaru
     autoload :Client, 'mushikago/hotaru/client'
     autoload :Request, 'mushikago/hotaru/request'
+
+    autoload :ListDomainRequest, 'mushikago/hotaru/list_domain_request'
+    autoload :CreateDomainRequest, 'mushikago/hotaru/create_domain_request'
+    autoload :DeleteDomainRequest, 'mushikago/hotaru/delete_domain_request'
+
+    autoload :ListDocumentRequest, 'mushikago/hotaru/list_document_request'
+    autoload :PutDocumentRequest, 'mushikago/hotaru/put_document_request'
+    autoload :DeleteDocumentRequest, 'mushikago/hotaru/delete_document_request'
+
+    autoload :ListCollocationRequest, 'mushikago/hotaru/list_collocation_request'
+    autoload :BuildCollocationRequest, 'mushikago/hotaru/build_collocation_request'
+    autoload :SearchCollocationRequest, 'mushikago/hotaru/search_collocation_request'
+    autoload :DeleteCollocationRequest, 'mushikago/hotaru/delete_collocation_request'
+    autoload :GetCollocationGraphRequest, 'mushikago/hotaru/get_collocation_graph_request'
   end
 end
