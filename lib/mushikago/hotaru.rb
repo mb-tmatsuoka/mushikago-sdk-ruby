511f677c1daaf939ed826fe776b1a607d145ff13
tmatsuoka <tmatsuoka@fa3f8e5a-24df-40e0-b2f8-b43c35aaeae2>
2012-02-16 12:08:42 +0000

    全APIのautoload列挙
    git-svn-id: svn+ssh://svn.miningbrownie.co.jp/srv/svn/mushikago/sdk/ruby/branches/0.2@2452 fa3f8e5a-24df-40e0-b2f8-b43c35aaeae2

--- lib/mushikago/hotaru.rb
+++ lib/mushikago/hotaru.rb
@@ -12,14 +12,32 @@ module Mushikago
 
     # text
     autoload :TextListRequest, 'mushikago/hotaru/text_list_request'
+    autoload :TextPutRequest, 'mushikago/hotaru/text_put_request'
+    autoload :TextDeleteRequest, 'mushikago/hotaru/text_delete_request'
+    autoload :TextTagsetRequest, 'mushikago/hotaru/text_tagset_request'
+    autoload :TextGetRequest, 'mushikago/hotaru/text_get_request'
 
     # classifier
+    autoload :ClassifierJudgeRequest, 'mushikago/hotaru/classifier_judge_request'
 
     # collocation
     autoload :CollocationListRequest, 'mushikago/hotaru/collocation_list_request'
+    autoload :CollocationDeleteRequest, 'mushikago/hotaru/collocation_delete_request'
+    autoload :CollocationGetRequest, 'mushikago/hotaru/collocation_get_request'
+    autoload :CollocationCreateRequest, 'mushikago/hotaru/collocation_create_request'
+    autoload :CollocationDownloadRequest, 'mushikago/hotaru/collocation_download_request'
+    autoload :CollocationWordcountRequest, 'mushikago/hotaru/collocation_wordcount_request'
+
+    # tag
+    autoload :TagListRequest, 'mushikago/hotaru/tag_list_request'
+
+    # word
+    autoload :WordListRequest, 'mushikago/hotaru/word_list_request'
+    autoload :WordGetRequest, 'mushikago/hotaru/word_get_request'
 
     # dictionary
     autoload :DictionaryPutRequest, 'mushikago/hotaru/dictionary_put_request'
     autoload :DictionaryListRequest, 'mushikago/hotaru/dictionary_list_request'
+    autoload :DictionaryDeleteRequest, 'mushikago/hotaru/dictionary_delete_request'
   end
 end
