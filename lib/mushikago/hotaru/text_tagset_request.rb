b8243c10bc60747358d821cbb3632c4b082acbaa
tmatsuoka <tmatsuoka@fa3f8e5a-24df-40e0-b2f8-b43c35aaeae2>
2012-02-19 07:48:08 +0000

    text/tagsetのバグ修正
    git-svn-id: svn+ssh://svn.miningbrownie.co.jp/srv/svn/mushikago/sdk/ruby/branches/0.2@2542 fa3f8e5a-24df-40e0-b2f8-b43c35aaeae2

--- lib/mushikago/hotaru/text_tagset_request.rb
+++ lib/mushikago/hotaru/text_tagset_request.rb
@@ -3,16 +3,16 @@ module Mushikago
   module Hotaru
     class TextTagsetRequest < Mushikago::Http::PostRequest
       def path; '/1/hotaru/text/tagset' end
-      requestreplaceeter :domain_name
-      requestreplaceeter :text_id
-      requestreplaceeter :tags do |v| [v].flatten.compact.join(',') end
-      requestreplaceeter :replace, :default=>false do |v| (v ? 1 : 0).to_s end
+      request_parameter :domain_name
+      request_parameter :text_id
+      request_parameter :tags do |v| [v].flatten.compact.join(',') end
+      request_parameter :replace, :default=>false do |v| (v ? 1 : 0).to_s end
 
       def initialize domain_name, text_id, tags, options={}
         super(options)
         self.domain_name = domain_name
         self.text_id = text_id
-        self.tags = domain_name
+        self.tags = tags
         self.replace = options[:replace]
       end
     end
