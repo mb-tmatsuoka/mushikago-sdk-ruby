6a3a774465542be1c73587f346616feecbd00221
Toru Matsuoka <t.matsuoka@miningbrownie.co.jp>
2012-04-13 17:13:03 +0900

    ( refs #3502 ) テストケース追加

---
+++ spec/mushikago/ponz/request_analysis_request_spec.rb
@@ -0,0 +1,20 @@
+# -*- coding : utf-8 -*-
+require 'spec_helper'
+
+describe Mushikago::Ponz::RequestAnalysisRequest do
+  context '.new("domain_name", "url")' do
+    subject{ Mushikago::Ponz::RequestAnalysisRequest.new('domain_name', 'url') }
+    it{ should be_kind_of(Mushikago::Http::PutRequest) }
+    its(:domain_name){ should == 'domain_name' }
+    its(:url){ should == 'url' }
+  end
+
+  context '.new("domain_name", "url", :charset => "utf-8", :tag => "tag")' do
+    subject{ Mushikago::Ponz::RequestAnalysisRequest.new('domain_name', 'url', :charset => 'utf-8', :tag => 'tag') }
+    it{ should be_kind_of(Mushikago::Http::PutRequest) }
+    its(:domain_name){ should == 'domain_name' }
+    its(:url){ should == 'url' }
+    its(:charset){ should == 'utf-8' }
+    its(:tag){ should == 'tag' }
+  end
+end
