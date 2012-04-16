8648335ea3cce147e467404a53b50ad2f63669ee
Yoko Matsumori <y.matsumori@miningbrownie.co.jp>
2012-04-16 16:44:59 +0900

    ( refs #3529 )リクエストファイルとspecファイルを追加しました

---
+++ lib/mushikago/ponz/get_analysis_request.rb
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+module Mushikago
+  module Ponz
+    class GetAnalysisRequest < Mushikago::Http::GetRequest
+      def path; "/1/ponz/domains/#{domain_name}/analysises/#{request_id}" end
+      attr_accessor :domain_name
+      attr_accessor :request_id
+      
+      def initialize domain_name, request_id, options={}
+        super(options)
+        self.domain_name = domain_name
+        self.request_id = request_id
+      end
+    end
+  end
+end
---
+++ spec/mushikago/ponz/get_analysis_request_spec.rb
@@ -0,0 +1,11 @@
+# -*- coding : utf-8 -*-
+require 'spec_helper'
+
+describe Mushikago::Ponz::GetAnalysisRequest do
+  context '.new("domain_name", "request_id")' do
+    subject{ Mushikago::Ponz::GetAnalysisRequest.new('domain_name', 'request_id')}
+    it{ should be_kind_of(Mushikago::Http::GetRequest)}
+    its(:domain_name){ should == 'domain_name'}
+    its(:request_id){ should == 'request_id'}
+  end
+end
