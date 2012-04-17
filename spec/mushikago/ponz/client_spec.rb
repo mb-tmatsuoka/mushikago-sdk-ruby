5a363d61bcdec15cb0b7bc036d861028a2e4f3aa
Toru Matsuoka <t.matsuoka@miningbrownie.co.jp>
2012-04-17 17:19:30 +0900

    client.craete_domainのテストを追加

--- spec/mushikago/ponz/client_spec.rb
+++ spec/mushikago/ponz/client_spec.rb
@@ -3,10 +3,21 @@ require 'spec_helper'
 
 describe Mushikago::Ponz::Client do
   subject{ Mushikago::Ponz::Client.new }
+  it{ should be_respond_to(:create_domain) }
+  it{ should be_respond_to(:request_analysis) }
+  it{ should be_respond_to(:get_analysis) }
   it{ should be_respond_to(:get_queue_size) }
 
+  context 'create_domain' do
+    it 'が呼ばれたとき、Mushikago::Ponz::CreateDomainRequestのインスタンスが生成され、send_requestに渡される' do
+      client, mock = create_client_and_request_mock
+      Mushikago::Ponz::CreateDomainRequest.should_receive(:new).with('domain_name', 'seed', {}).and_return(mock)
+      client.create_domain('domain_name', 'seed')
+    end
+  end
+
   context 'get_queue_size' do
-    it 'が呼ばれたとき、Mushikago::Ponz::GetQueueSizeRequest.newのインスタンスが生成され、send_requestに渡される' do
+    it 'が呼ばれたとき、Mushikago::Ponz::GetQueueSizeRequestのインスタンスが生成され、send_requestに渡される' do
       client, mock = create_client_and_request_mock
       Mushikago::Ponz::GetQueueSizeRequest.should_receive(:new).with('domain_name', {}).and_return(mock)
       client.get_queue_size('domain_name')
