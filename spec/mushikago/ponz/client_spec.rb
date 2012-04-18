327d50d97ed3fbef0df29142afea997e2b000b64
hiromi suzuki <h.suzuki@miningbrownie.co.jp>
2012-04-18 17:51:51 +0900

    refs #3484 クライアントのテストを追加

--- spec/mushikago/ponz/client_spec.rb
+++ spec/mushikago/ponz/client_spec.rb
@@ -7,12 +7,14 @@ describe Mushikago::Ponz::Client do
   it{ should be_respond_to(:request_analysis) }
   it{ should be_respond_to(:get_analysis) }
   it{ should be_respond_to(:get_queue_size) }
+  it{ should be_respond_to(:get_information) }
 
   [
     [:create_domain, Mushikago::Ponz::CreateDomainRequest, ['domain_name', 'seed']],
     [:request_analysis, Mushikago::Ponz::RequestAnalysisRequest, ['domain_name', 'url']],
     [:get_analysis, Mushikago::Ponz::GetAnalysisRequest, ['domain_name', 'request_id']],
     [:get_queue_size, Mushikago::Ponz::GetQueueSizeRequest, ['domain_name']],
+    [:get_information, Mushikago::Ponz::GetInformationRequest, ['domain_name']],
   ].each do |method_name, clazz, args|
     context method_name do
       it "が呼ばれたとき、#{clazz}のインスタンスが生成され、send_requestに渡される" do
