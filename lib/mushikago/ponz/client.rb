9a0959f035d5ad14706adb9d3202674e3a6ed3a2
karie <k.arie@miningbrownie.co.jp>
2012-04-23 13:57:38 +0900

    refs #3493 クライアントにドメイン一覧取得を追加

--- lib/mushikago/ponz/client.rb
+++ lib/mushikago/ponz/client.rb
@@ -79,6 +79,21 @@ module Mushikago
         request = Ponz::GetInformationRequest.new(domain_name, options)
         send_request(request)
       end
+
+      # ドメインの一覧を取得する
+      #
+      # @example
+      #    client.get_domain_list(20, 10, 'ec')
+      #
+      # @param [Hash] options リクエストのオプション
+      # @option options [Integer] : limit 最大取得件数
+      # @option options [Integer] : offset 開始位置
+      # @option options [String] : filter 検索文字（先頭一致）
+      # @return [Mushikago::Http::Response] リクエストの結果
+      def get_information options={}
+        request = Ponz::GetDomainListRequest.new(options)
+        send_request(request)
+      end
     end
   end
 end
