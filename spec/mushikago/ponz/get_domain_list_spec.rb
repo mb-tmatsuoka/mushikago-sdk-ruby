31de1a34aff576354c0fe624110b6afc967c335c
karie <k.arie@miningbrownie.co.jp>
2012-04-23 14:33:03 +0900

    refs #3493 リクエストクラスのテストにドメイン一覧取得を追加

--- spec/mushikago/ponz/get_domain_list_spec.rb
+++ spec/mushikago/ponz/get_domain_list_spec.rb
@@ -8,8 +8,8 @@ describe Mushikago::Ponz::GetDomainListRequest do
     its(:path){ should == '/1/ponz/domains' }
   end
 
-  context '.new(:limit => 20, :offset => 10,:filter => 'domain')' do
-    subject{ Mushikago::Ponz::CreateDomainRequest.new(:limit => 20, :offset => 10,:filter => 'domain') }
+  context '.new(:limit => 20, :offset => 10,:filter => "domain")' do
+    subject{ Mushikago::Ponz::GetDomainListRequest.new(:limit => 20, :offset => 10,:filter => 'domain') }
     it{ should be_kind_of(Mushikago::Http::GetRequest) }
     its(:limit){ should == 20 }
     its(:offset){ should == 10 }
