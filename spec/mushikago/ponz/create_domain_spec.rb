215b7714a4f0f8ad1db9c7d351c48a383770af4a
Toru Matsuoka <t.matsuoka@miningbrownie.co.jp>
2012-04-24 11:27:04 +0900

    ( refs #3641 ) CreateDomainRequestにschema追加

--- spec/mushikago/ponz/create_domain_spec.rb
+++ spec/mushikago/ponz/create_domain_spec.rb
@@ -2,20 +2,32 @@
 require 'spec_helper'
 
 describe Mushikago::Ponz::CreateDomainRequest do
-  context '.new("domain_name", "seed")' do
-    subject{ Mushikago::Ponz::CreateDomainRequest.new('domain_name', 'seed') }
-    it{ should be_kind_of(Mushikago::Http::PutRequest) }
-    its(:domain_name){ should == 'domain_name' }
-    its(:seed){ should == 'seed' }
-    its(:path){ should == '/1/ponz/domains/domain_name' }
+  before :all do
+    @schema = Mushikago::Ponz::Schema.new do
+      add Mushikago::Ponz::Field.new('name', true, :string)
+      add Mushikago::Ponz::Field.new('price', true, :number)
+      add Mushikago::Ponz::Field.new('shipping', false, :string)
+      add Mushikago::Ponz::Field.new('description', false, :string)
+    end
   end
 
-  context '.new("domain_name", "seed", :description => "description")' do
-    subject{ Mushikago::Ponz::CreateDomainRequest.new('domain_name', 'seed', :description => 'description') }
+  shared_examples_for 'a valid request instance' do |n, s, o|
+    subject{ Mushikago::Ponz::CreateDomainRequest.new(n, s, @schema, o) }
     it{ should be_kind_of(Mushikago::Http::PutRequest) }
-    its(:domain_name){ should == 'domain_name' }
-    its(:seed){ should == 'seed' }
-    its(:description){ should == 'description' }
+    its(:path){ should == "/1/ponz/domains/#{n}" }
+    its(:domain_name){ should == n }
+    its(:seed){ should == s }
+    its(:schema){ should == @schema.to_json }
+    its(:description){ should == o[:description] }
+  end
+
+  test_parameters = [
+    ['domain_name', 'seed', {}],
+    ['name', 'seed', {:description => 'description'}],
+  ].each do |n, s, o|
+    context ".new(#{n}, #{s}, #{o})" do
+      it_should_behave_like 'a valid request instance', n, s, o
+    end
   end
 end
 
