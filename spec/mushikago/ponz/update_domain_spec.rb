7679091dd1711bd1862999e72fb62c27ea7daa4f
karie <k.arie@miningbrownie.co.jp>
2012-05-01 09:33:29 +0900

    ( refs #3475 ) rspec のshared_example が他と被ってjenkinsでエラーになっていたため、修正しました

--- spec/mushikago/ponz/update_domain_spec.rb
+++ spec/mushikago/ponz/update_domain_spec.rb
@@ -2,7 +2,7 @@
 require 'spec_helper'
 
 describe Mushikago::Ponz::UpdateDomainRequest do
-  shared_examples_for 'a valid request instance' do |n, d, o|
+  shared_examples_for ' a valid request instance for update_domain' do |n, d, o|
     subject{ Mushikago::Ponz::UpdateDomainRequest.new(n, d, o) }
     it{ should be_kind_of(Mushikago::Http::PostRequest) }
     its(:path){ should == "/1/ponz/domains/#{n}" }
@@ -15,7 +15,7 @@ describe Mushikago::Ponz::UpdateDomainRequest do
     ['name', 'description', {}],
   ].each do |n, d, o|
     context ".new(#{n}, #{d}, #{o})" do
-      it_should_behave_like 'a valid request instance', n, d, o
+      it_should_behave_like ' a valid request instance for update_domain', n, d, o
     end
   end
 end
