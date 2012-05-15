0e3ff2cfa66a043b7911ac3803bbe4152064e80b
Toru Matsuoka <t.matsuoka@miningbrownie.co.jp>
2012-05-15 10:02:31 +0900

    ( refs #3661 ) training_dataのフォーマット修正

--- lib/mushikago/hanamgri/training_data.rb
+++ lib/mushikago/hanamgri/training_data.rb
@@ -6,20 +6,20 @@ module Mushikago
       attr_reader :training_data
       
       def initialize(&block)
-        @training_data = []
+        @training_data = {}
         instance_eval(&block) if block_given?
       end
       
-      def add field_or_hash
-        element = field_or_hash
-        if element.kind_of?(Hash)
-          element = Mushikago::Hanamgri::Element.new(element[:key], element[:value])
-        end
-        @training_data << element
+      def []= key, value
+        @training_data[key] = value
+      end
+
+      def put key, value
+        @training_data[key] = value
       end
 
       def to_json *args
-        {:training_data => @training_data}.to_json(args)
+        @training_data.to_json(args)
       end
     end
   end
