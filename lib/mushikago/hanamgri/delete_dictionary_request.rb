82514f8b4652b35a4d873d580ad58e735e11a901
Toru Matsuoka <t.matsuoka@miningbrownie.co.jp>
2012-05-17 09:13:19 +0900

    ( refs #3810 ) リクエストパラメータにdictionary_nameが含まれてないバグを修正

--- lib/mushikago/hanamgri/delete_dictionary_request.rb
+++ lib/mushikago/hanamgri/delete_dictionary_request.rb
@@ -3,7 +3,7 @@ module Mushikago
   module Hanamgri
     class DeleteDictionaryRequest < Mushikago::Http::DeleteRequest
       def path; "/1/hanamgri/dictionary" end
-      attr_accessor :dictionary_name
+      request_parameter :dictionary_name
       
       def initialize dictionary_name, options={}
         super(options)
